937e0d7124f14412f96c50936b44c31de797f417
Cameron Quinn <cameronquinn@Camerons-MacBook-Pro.local>
2019-10-23 18:19:39 +0200

    Played around with styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,23 @@
+const template = document.createElement('template')
+template.innerHTML = `
+
+
+`
 function main () {
   // Set up a menu on the bottom with links to launch apps
   // Create divs to contain applications that has a close feature
+  const instaChatButton = document.getElementById('instachat')
+  instaChatButton.addEventListener('click', (event) => {
+    console.log('instaChat')
+  })
+  const memoryGameButton = document.getElementById('memorygame')
+  memoryGameButton.addEventListener('click', (event) => {
+    console.log('memoryGame')
+  })
+  const clickGameButton = document.getElementById('clickgame')
+  clickGameButton.addEventListener('click', (event) => {
+    console.log('clickGame')
+  })
   /*
   const instaChat = document.createElement('insta-chat')
   const appendAt = document.querySelector('body')
@@ -10,7 +27,7 @@ function main () {
 }
 
 function createWindow() {
-  
+
 
 }
 
